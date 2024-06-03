f0b90344da62ab395b2105bc346c82d3be305838
Vadimcha <BelovBadim2014@yandex.ru>
2024-06-03 17:38:53 +0300

    Add some styles

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,8 @@
-import styles from './Home.module.scss'
+import styles from './Home.module.scss';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import {useState} from "react";
-import {SendQuestion} from "../../api_requests/SendQuestion.ts";
+import { useState } from "react";
+import { SendQuestion } from "../../api_requests/SendQuestion.ts";
+import './Home.scss'
 
 export interface IMessage {
   role: "user" | "assistant";
@@ -9,20 +10,41 @@ export interface IMessage {
 }
 
 const Home = () => {
-  const [question, setQuestion] = useState<string>("")
-  const [history, setHistory] = useState<IMessage[]>([])
+  const [question, setQuestion] = useState<string>("");
+  const [history, setHistory] = useState<IMessage[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const send = () => {
+    SendQuestion(question, history)
+      .then(res => {
+        setHistory(prevState => [...prevState, { role: "assistant", message: res }]);
+        setLoading(false);
+      });
+  };
+
+  const handleSend = () => {
+    setLoading(true)
+    setHistory(prevState => [...prevState, { role: "user", message: question }]);
+    send();
+    setQuestion(""); // Очистить поле ввода после отправки
+  };
+
   return (
     <div className={styles.content}>
-
-      <div className={styles.dialog}>
+      <div className={styles.dialog} id="dialog">
         {history.map((answer, index) => (
           <SyntaxHighlighter
+            wrapLongLines
+            wrapLines
             useInlineStyles
             language="javascript"
             key={index}
             className={styles.message}
-          >{answer.message}</SyntaxHighlighter>
+          >
+            {answer.message}
+          </SyntaxHighlighter>
         ))}
+        { loading && <div className={styles.loader}/> }
       </div>
       <div className={styles.input_container}>
         <input
@@ -32,16 +54,10 @@ const Home = () => {
           className={styles.input}
           placeholder={"Введите запрос"}
         />
-        <button onClick={() => {
-          history.push({ role: "user", message: question })
-          SendQuestion(question, history)
-            .then(res =>
-              setHistory(prevState => [...prevState, { role: "assistant", message: res }])
-            )
-        }}>Send
+        <button className={styles.btn} onClick={handleSend}>
+          Send
         </button>
       </div>
-
     </div>
   );
 };
